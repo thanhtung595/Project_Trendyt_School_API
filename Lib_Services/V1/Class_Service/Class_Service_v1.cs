205d56576b746654a0a0eb67a3a8b7a1178bc874
Thanh Tung <nguyenthanhtung.vn.06112003@gmail.com>
2024-03-09 15:06:55 +0700

    #21 ChucNangEditClass Thay đổi tên class Thay đổi chủ nhiệm Thay đổi tag

--- Lib_Services/V1/Class_Service/Class_Service_v1.cs
+++ Lib_Services/V1/Class_Service/Class_Service_v1.cs
@@ -1,9 +1,12 @@
 ﻿using App_DataBaseEntity.DbContextEntity_SQL_Sever;
 using App_Models.Models_Table_CSDL;
+using Lib_Models.Model_Update.Class;
+using Lib_Models.Models_Insert.v1.Class_School;
 using Lib_Models.Models_Select.Class;
 using Lib_Models.Status_Model;
 using Lib_Repository.V1.Class_Repository;
 using Lib_Services.Token_Service;
+using Lib_Services.V1.Class_Member_Service;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,12 +21,14 @@ namespace Lib_Services.V1.Class_Service
         private readonly Trendyt_DbContext _db;
         private readonly IClass_Repository_v1 _class_Repository_V1;
         private readonly IToken_Service_v1 _token_Service_V1;
+        private readonly IClass_Member_Service_v1 _class_Member_Service_V1;
         public Class_Service_v1(Trendyt_DbContext db , IClass_Repository_v1 class_Repository_V1,
-            IToken_Service_v1 token_Service_V1)
+            IToken_Service_v1 token_Service_V1, IClass_Member_Service_v1 class_Member_Service_V1)
         {
             _db = db;
             _class_Repository_V1 = class_Repository_V1;
             _token_Service_V1 = token_Service_V1;
+            _class_Member_Service_V1 = class_Member_Service_V1;
         }
 
         #region SelectAll
@@ -83,9 +88,67 @@ namespace Lib_Services.V1.Class_Service
         #endregion
 
         #region UpdateAsync
-        public Task<Status_Application> UpdateAsync(string name_ClassSchool)
+        public async Task<Status_Application> UpdateAsync(Class_Update_v1 request)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(request.tag))
+            {
+                request.tag = "active";
+            }
+
+            int id_AccountManager = await _token_Service_V1.GetAccessTokenIdAccount();
+            var memberManager = await _db.tbMenberSchool.FirstOrDefaultAsync(x => x.id_Account == id_AccountManager);
+            // Kiểm tra class có tồn tại và name class không tồn tại
+
+            var isIdClass = await _db.tbClassSchool.FindAsync(request.id_ClassSchool);
+            if (isIdClass == null)
+            {
+                return new Status_Application { StatusBool = false, StatusType = "Id lớp không tồn tại" };
+            }
+            var checkNameClass = await _db.tbClassSchool.FirstOrDefaultAsync(x => x.id_KhoaSchool == memberManager!.id_KhoaSchool
+                                && x.id_ClassSchool != isIdClass.id_ClassSchool && x.name_ClassSchool!.ToLower() == request.name_ClassSchool!.ToLower());
+            if (checkNameClass != null)
+            {
+                return new Status_Application { StatusBool = false, StatusType = "Tên lớp đã tồn tại" };
+            }
+            //KIểm tra teacher
+            if (request.id_Teacher != 0)
+            {
+                // Kiểm tra trong lớp đã tồn tại teacher
+                var teacher_Class = await (from mc in _db.tbClassSchool_Menber
+                                           where mc.id_ClassSchool == request.id_ClassSchool
+                                           join m in _db.tbMenberSchool
+                                           on mc.id_MenberSchool equals m.id_MenberSchool
+                                           join r in _db.tbRoleSchool
+                                           on m.id_RoleSchool equals r.id_RoleSchool
+                                           where r.name_Role == "teacher"
+                                           select new { memberClass = mc, Member = m, Role = r }).FirstOrDefaultAsync();
+
+                if (teacher_Class == null)
+                {
+                    Class_Member_Insert_v1 _Insert_V1 = new Class_Member_Insert_v1
+                    {
+                        id_ClassSchool = request.id_ClassSchool,
+                        id_Student = request.id_Teacher
+                    };
+                    Status_Application status = await _class_Member_Service_V1.Insert(_Insert_V1);
+                    if (!status.StatusBool)
+                    {
+                        return status;
+                    }
+                }
+                else
+                {
+                     teacher_Class.memberClass.id_MenberSchool = request.id_Teacher;
+                }
+            }
+            else
+            {
+                return new Status_Application { StatusBool = false, StatusType = "Id teacher 0 không tồn tại" };
+            }
+            isIdClass.name_ClassSchool = request.name_ClassSchool!.Trim();
+            isIdClass.tags = request.tag;
+            await _db.SaveChangesAsync();
+            return new Status_Application { StatusBool = true, StatusType = "success" };
         }
         #endregion
     }
