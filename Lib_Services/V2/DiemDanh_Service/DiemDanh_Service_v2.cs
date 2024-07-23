20bc8938e1b374726821bf4509d6863a30fdb021
Thanh Tung <nguyenthanhtung.vn.06112003@gmail.com>
2024-07-23 23:02:02 +0700

    #106 API diemdanh thoiGianBatDau - students

--- Lib_Services/V2/DiemDanh_Service/DiemDanh_Service_v2.cs
+++ Lib_Services/V2/DiemDanh_Service/DiemDanh_Service_v2.cs
@@ -20,35 +20,72 @@ namespace Lib_Services.V2.DiemDanh
         private readonly Trendyt_DbContext _db;
         private readonly IRepository<tbDiemDanh> _repositoryDiemDanh;
         private readonly IRepository<tbMonHoc> _repositoryMonHoc;
-        public DiemDanh_Service_v2(IRepository<tbDiemDanh> repositoryDiemDanh, IRepository<tbMonHoc> repositoryMonHoc, Trendyt_DbContext db)
+        private readonly IRepository<tbLichHoc> _repositoryLichHoc;
+        public DiemDanh_Service_v2(IRepository<tbDiemDanh> repositoryDiemDanh, IRepository<tbMonHoc> repositoryMonHoc, Trendyt_DbContext db,
+            IRepository<tbLichHoc> repositoryLichHoc)
         {
             _repositoryDiemDanh = repositoryDiemDanh;
             _repositoryMonHoc = repositoryMonHoc;
             _db = db;
+            _repositoryLichHoc = repositoryLichHoc;
         }
 
-        public async Task<List<IGrouping<int, DiemDanh_Select_v1>>> GetDiemDanhMonHocAsync(int idMonHoc)
+        //public async Task<List<IGrouping<DateTime, DiemDanh_Select_v1>>> GetDiemDanhMonHocAsync(int idMonHoc)
+        //{
+        //    var query = await _repositoryDiemDanh.GetAllIncluding(dd => dd.tbLichHoc!.id_MonHoc == idMonHoc, ms => ms.tbMonHocClass_Student!.tbMenberSchool!.tbAccount!);
+        //    if (!query.Any())
+        //    {
+        //        return null!;
+        //    }
+
+        //    var result = query.Select(d => new DiemDanh_Select_v1
+        //    {
+        //        ThoiGianBatDau = (from x in _db.tbLichHoc
+        //                          where x.id_LichHoc == d.id_LichHoc
+        //                          select x.thoiGianBatDau).FirstOrDefault(),
+        //        Students = new List<DiemDanhStudent_Select>
+        //        {
+        //            new DiemDanhStudent_Select
+        //            {
+        //                id_DiemDanh = d.id_DiemDanh,
+        //                id_LichHoc = d.id_LichHoc
+        //            }
+        //        }
+        //    }).ToList();
+
+        //    //Nhóm các result với id_LichHoc
+        //    var groupResult = result.GroupBy(d => d.ThoiGianBatDau).ToList();
+        //    return groupResult;
+        //}
+        public async Task<List<DiemDanh_Select_v1>> GetDiemDanhMonHocAsync(int idMonHoc)
         {
-            var query = await _repositoryDiemDanh.GetAllIncluding(dd => dd.tbLichHoc!.id_MonHoc == idMonHoc, ms => ms.tbMonHocClass_Student!.tbMenberSchool!.tbAccount!);
-            if (!query.Any())
+            var lichHoc = await _repositoryLichHoc.GetAll(x => x.id_MonHoc == idMonHoc);
+            if (!lichHoc.Any())
             {
                 return null!;
             }
-
-            var result = query.Select(d => new DiemDanh_Select_v1
+            var result = lichHoc.Select(d => new DiemDanh_Select_v1
             {
-                id_DiemDanh = d.id_DiemDanh,
-                msv = d.tbMonHocClass_Student!.id_MenberSchool,
-                fullName = d.tbMonHocClass_Student!.tbMenberSchool!.tbAccount!.fullName,
-                _CuoiGio = d._CuoiGio,
-                _DauGio = d._DauGio,
-                _DiMuon = d._DiMuon,
-                id_LichHoc = d.id_LichHoc,
+                ThoiGianBatDau = d.thoiGianBatDau,
+                Students = (from s in _db.tbDiemDanh
+                            join sm in _db.tbMonHocClass_Student
+                            on s.id_MonHocClass_Student equals sm.id_MonHocClass_Student
+                            join m in _db.tbMenberSchool
+                            on sm.id_MenberSchool equals m.id_MenberSchool
+                            join ac in _db.tbAccount
+                            on m.id_Account equals ac.id_Account
+                            where s.id_LichHoc == d.id_LichHoc
+                            select new DiemDanhStudent_Select
+                            {
+                                id_DiemDanh = s.id_DiemDanh,
+                                id_LichHoc = s.id_LichHoc,
+                                _CuoiGio = s._CuoiGio,
+                                _DauGio = s._DauGio,
+                                msv = m.id_MenberSchool,
+                                fullName = ac.fullName
+                            }).ToList(),
             }).ToList();
-
-            //Nhóm các result với id_LichHoc
-            var groupResult = result.GroupBy(d => d.id_LichHoc).ToList();
-            return groupResult;
+            return result;
         }
 
         public async Task<Status_Application> InsertAsync(List<int> idLichHoc, List<int> idStudent)
